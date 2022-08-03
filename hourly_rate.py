2a2f1aee55b9fd8c26ac1069a868079378fa8ca6
Nick Simms <nsimms@asu.edu>
2022-08-02 21:34:48 -0700

    first commit

---
+++ hourly_rate.py
@@ -0,0 +1,13 @@
+# 2.3 Write a program to prompt the user for the hours and rate per hour using input to
+# compute the gross pay. Use 35 hours and a rate of 2.75 per hour to test the program
+# (the pay should be 96.25). You should input to read a string and float() to
+# convert the string to a number. Do not worry about error checking or bad user
+# data.
+
+# This first line is provided for you.
+
+hrs = input("Enter Hours:")
+rate = input("Enter rate per hour:")
+
+output = float(hrs) * float(rate)
+print(output)
